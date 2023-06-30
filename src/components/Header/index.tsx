595b402748f424abfe5d15e3ed60bdf1139aaa4e
Clovis antunes <clovissantannaa@gmail.com>
2023-06-30 16:48:18 -0300

    ajustando organização

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logoImg from '../../assets/logo.png';
+import pickle from '../../assets/pickleRick.png';
+
+export default function Navbar(){
+    return(
+        <nav className={styles.navbar}>
+            <div className={styles.logoContainer}>
+                <Image src={logoImg} alt='Logo Rick and Morty' />
+            </div>
+            <div className={styles.pickleRick}>
+                <Image src={pickle} alt='Pickle Rick' />
+            </div>
+        </nav>
+    )
+}
