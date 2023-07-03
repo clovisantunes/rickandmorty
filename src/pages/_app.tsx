91e0037bb9448dfb27cb4226be47085e18a95041
Clovis antunes <clovissantannaa@gmail.com>
2023-07-03 10:54:59 -0300

    Organizando o codigo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
-import { AuthProvider } from '@/contexts/AuthContext';
 function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
+    
       <Component {...pageProps} />
-    </AuthProvider>
+
   );
 }
 export default App;
